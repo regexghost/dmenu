ba36bbcd2cce2e0a4cab68885d672d72c966861b
regexghost <regexghost@protonmail.com>
2026-04-23 09:01:09 +0100

    vi mode

--- config.def.h
+++ config.def.h
@@ -21,6 +21,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeCaret] = { "#eeeeee", "#222222" },
+	[SchemeCursor] = { "#222222", "#bbbbbb"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -30,6 +31,18 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * -vi option; if nonzero, vi mode is always enabled and can be
+ * accessed with the global_esc keysym + mod mask
+ */
+static unsigned int vi_mode = 1;
+static unsigned int start_mode = 0;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
+static Key quit_keys[] = {
+	/* keysym	modifier */
+	{ XK_q,		0 }
+};
 
 /* Size of the window border */
 static unsigned int border_width = 0;
